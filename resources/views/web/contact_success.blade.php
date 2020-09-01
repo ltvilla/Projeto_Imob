314a90c53849db8ecd292624dcb167b695eef176
Tiago Villa <tiago_villa1@hotmail.com>
2020-09-01 07:23:05 -0300

    Alteração de Texto

--- resources/views/web/contact_success.blade.php
+++ resources/views/web/contact_success.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container p-5">
-        <h2 class="text-center bg-white text-front">Seu contato foi enviado com <b>SUCESSO</b></h2>
+        <h2 class="text-center bg-white text-front">Seu contato foi enviado com <strong>SUCESSO</strong></h2>
         <p class=" text-center"><a href="{{ url()->previous() }}" class="text-front">... Continuar navegando!</a></p>
     </div>
 @endsection
